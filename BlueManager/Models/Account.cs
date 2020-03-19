9148d33e8602f168779622bcefc0503ee803bdf8
Wojciech Rak <wojciechrak@users.noreply.github.com>
2020-03-19 20:00:27 +0100

    Login form fixes

--- BlueManager/Models/Account.cs
+++ BlueManager/Models/Account.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace BlueManagerPlatform.Models
+﻿namespace BlueManager.Models
 {
     public class Account
     {
